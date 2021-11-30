51c6ccf120986e0351e84aafa9d90e3c17a587e6
姜宽 <jiangkuan@zhugeio.com>
2021-12-01 00:38:16 +0800

    二分查找（下标查找和左侧边界查找）

--- algorithm/binary/1.js
+++ algorithm/binary/1.js
@@ -1,4 +1,51 @@
 /**
  * 二分查找
+ * 寻找左边界
+ * 不断向左收缩，达到锁定左侧边界的目的。
  */
 
+const nums = [1,2,3,4,5,6,7]
+const target = 2
+
+function binarySearch(nums = [], target) {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    const middle = Math.floor((right - left) / 2) + left // 计算中间值
+    const value = nums[middle]
+    if (value > target) {
+      right = middle - 1 // 如果中间值大于目标值 说明目标在左侧 故收紧右边界
+    } else if (value < target) {
+      left = middle + 1 // 如果中间值小于目标值 说明目标在右侧 故收紧左边界
+    } else if (value === target) {
+      return middle
+    }
+  }
+  return -1
+}
+
+console.log(binarySearch(nums, target))
+
+const nums1 = [1,2,3,4,5,6,6,6,6,6,6,6,6,7,8]
+function binarySearchLeftBound(nums = [], target) {
+  let left = 0
+  let right = nums.length -1
+  
+  while (left <= right) {
+    const middle = Math.floor((right - left) / 2) + left
+    console.log(left, right)
+    const value = nums[middle]
+    if (value === target) {
+      right = middle - 1
+    } else if (value < target) {
+      left = middle + 1
+    } else if (value > target) {
+      right = middle - 1
+    }
+  }
+  if (left >= nums.length || nums[left] !== target) {
+    return -1
+  }
+  return left
+}
+console.log(binarySearchLeftBound(nums1, 0))
