03e29e570c6eee70dd80d3524182f5f42a2b83f4
jiangkuan <jiangkuan@shuidi-inc.com>
2021-12-01 17:04:07 +0800

    二分查找（右边界查询）

--- algorithm/binary/1.js
+++ algorithm/binary/1.js
@@ -8,6 +8,9 @@ const nums = [1,2,3,4,5,6,7]
 const target = 2
 
 function binarySearch(nums = [], target) {
+  if (nums.length === 0) {
+    return -1
+  }
   let left = 0
   let right = nums.length - 1
   while (left <= right) {
@@ -28,6 +31,9 @@ console.log(binarySearch(nums, target))
 
 const nums1 = [1,2,3,4,5,6,6,6,6,6,6,6,6,7,8]
 function binarySearchLeftBound(nums = [], target) {
+  if (!nums.length) {
+    return -1
+  }
   let left = 0
   let right = nums.length -1
   
@@ -49,3 +55,25 @@ function binarySearchLeftBound(nums = [], target) {
   return left
 }
 console.log(binarySearchLeftBound(nums1, 0))
+
+function binarySearchRightBound(list = [], target) {
+  let left = 0
+  let right = list.length - 1
+  while (left <= right) {
+    const middle = Math.floor((right - left) / 2) + left
+    const value = list[middle]
+    if (value === target) {
+      left = middle + 1
+    } else if (value > target) {
+      right = middle - 1
+    } else if (value < target) {
+      left = middle + 1
+    }
+  }
+  if (right === list.length - 1 && list[right] !== target) {
+    return -1
+  }
+  return right
+}
+// const nums1 = [1,2,3,4,5,6,6,6,6,6,6,6,6,7,8]
+console.log('binarySearchRightBound', binarySearchRightBound(nums1, 6))
