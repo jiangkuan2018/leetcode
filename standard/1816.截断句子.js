5d095d40fbb98bfb751deb632887dec1efb82432
姜宽 <jiangkuan@zhugeio.com>
2021-12-07 22:03:24 +0800

    截断句子

---
+++ standard/1816.截断句子.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1816 lang=javascript
+ *
+ * [1816] 截断句子
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+  return s.split(' ').slice(0, k).join(' ')
+};
+// @lc code=end
+
